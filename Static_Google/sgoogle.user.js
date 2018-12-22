48cae320d0349c02d4ba32d41a9ade1d71e3fcb5
tumpio <tumpio@post.com>
2018-12-22 11:46:38 +0200

    Update static google

--- Static_Google/sgoogle.user.js
+++ Static_Google/sgoogle.user.js
@@ -2,32 +2,23 @@
 // @name            Static Google
 // @description     Google search interface always visible on screen, and some additional changes for more compact layout.
 // @author          tumpio
-// @oujs:author     tumpio
 // @namespace       tumpio@sci.fi
 // @homepageURL     https://openuserjs.org/scripts/tumpio/Static_Google
 // @supportURL      https://github.com/tumpio/gmscripts
-// @source          https://userstyles.org/styles/99153
-// @icon            https://raw.githubusercontent.com/tumpio/gmscripts/master/Static_Google/large.png
-// @resource        css http://userstyles.org/styles/99153.css#md5=cc035d8beb1114c99826212c0dfaab86
+// @icon            https://raw.githubusercontent.com/tumpio/googlestatic/master/icon.svg
+// @resource        css https://raw.githubusercontent.com/tumpio/googlestatic/master/static-style.css
 // @include         http://www.google.*
 // @include         https://www.google.*
 // @match           https://encrypted.google.com/*
 // @noframes
 // @run-at          document-start
-// @grant           GM_addStyle
-// @grant           GM_getResourceText
-// @version         1.9.8
+// @grant           GM.getResourceUrl
+// @version         1.10.0
 // ==/UserScript==
 
-// Static css, from userstyle.org, remove @-moz-document
-var css = GM_getResourceText("css").replace(/@-moz-document.*{([^]+)}/, "$1");
-
-// Fix for Scriptish issue #90
-// https://github.com/scriptish/scriptish/issues/90
-if (document.head) // Greasemonkey
-    GM_addStyle(css);
-else { // Scriptish
-    document.onreadystatechange = function () {
-        GM_addStyle(css);
-    };
-}
+(async function() {
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = await GM.getResourceUrl("css");
+    document.head.appendChild(link);
+})();
